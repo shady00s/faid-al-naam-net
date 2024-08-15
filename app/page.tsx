471b6d058a1cb6d1000c0d390007a94900d6c3f7
shady00s <ssk2310@hotmail.com>
2024-08-15 17:35:16 +0300

    fix loading

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,6 @@ import SplashScreen from './[locale]/components/splash_screen';
 export default function RootPage() {
 
   const router = useRouter();
-
-  // Simulate data loading or any other delay
-  setTimeout(() => {
-    router.push('/ar');
-  }, 5000);
   return <div><SplashScreen></div>
   
 }
