460d9c5a5f99ac4beaee63d4994d22b4f67c46f1
shady00s <ssk2310@hotmail.com>
2024-08-25 15:13:40 +0300

    fixing multiple inputs

--- app/[locale]/components/increamenting_input.tsx
+++ app/[locale]/components/increamenting_input.tsx
@@ -1,28 +1,42 @@
-"use client"
-import { useEffect, useState } from "react"
+import { memo, useCallback, useState, useEffect } from "react";
 
-interface increamentInterface {
-    component:JSX.Element
+interface IncreamentingInputWidgetProps {
+  component: JSX.Element;
 }
-export default function IncreamentingInputWidget({component}:increamentInterface){
-    const [components,setComponent] = useState<JSX.Element[]>([])
-        useEffect(()=>{
-            setComponent(()=>[<div key={Math.random()}>{component}</div>])          
-        },[component])
 
-        function addNewComponent(){
-            setComponent((prev)=> [...prev,<div key={Math.random()}>{component}</div>])
-        }    
-    return <div className="flex items-end w-full">
-            <div className="flex flex-col  w-full">
-                {components.map((e)=>e)}
-            </div>
+// Wrap the `component` prop in a separate memoized component
+const MemoizedComponent = memo(({ component }: { component: JSX.Element }) => {
+  return component;
+});
 
-            <button className="px-3 pb-1 text-center rounded-sm   text-white text-2xl bg-blue-400 my-1 mx-2 " onClick={(e)=>{
-                e.preventDefault();
-                addNewComponent();
-                
-                }}>+</button>
+function IncreamentingInputWidget({ component }: IncreamentingInputWidgetProps) {
+  const [components, setComponents] = useState<JSX.Element[]>([]);
+
+  const increment = useCallback(() => {
+    if(components.length == 0){
+        setComponents(()=>[<MemoizedComponent key={Math.random()} component={component} />])
+    }
+  }, [component]);
+
+  useEffect(() => {
+    increment();
+  }, [component]); // Dependency for increment
+
+  function addNewComponent() {
+    setComponents(prevComponents => [...prevComponents, <MemoizedComponent key={prevComponents.length} component={component} />]);
+  }
+
+  return (
+    <div className="flex items-end w-full">
+      <div className="flex flex-col w-full">{components}</div>
+      <button
+        className="px-3 pb-1 text-center rounded-sm text-white text-2xl bg-blue-400 my-1 mx-2"
+        onClick={addNewComponent}
+      >
+        +
+      </button>
     </div>
-
+  );
 }
+
+export default memo(IncreamentingInputWidget);
